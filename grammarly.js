d088d29fa63a1e31a5faad806a94072c63fcb8d6
Weijia Sun <swj1442291549@gmail.com>
2021-11-07 15:38:50 +0800

    Minor changes

--- grammarly.js
+++ grammarly.js
@@ -1,6 +1,4 @@
 let obj = JSON.parse($response.body)
 obj.type = "Premium";
 obj.free = false;
-obj.plagiarismOn = true;
-obj.institutionPlagiarismDisabled = false;
 $done({body:JSON.stringify(obj)});
